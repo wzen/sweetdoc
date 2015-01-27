95e2043c1d52f653951347b8e89ee7ab8aad0b2a
wzen <wzensum@gmail.com>
2015-01-28 08:40:33 +0900

    タイムラインイベント 一時コミット

--- app/controllers/worktable_controller.rb
+++ app/controllers/worktable_controller.rb
@@ -12,21 +12,21 @@ class WorktableController < ApplicationController
     @select_forms << {name: '(Select)', value: ''}
     common_events.each do |c|
       name = nil
-      unless !c.localize_common_action_events.empty?
+      unless c.localize_common_action_events.empty?
         l_events = c.localize_common_action_events
         l_events.each do |l|
           if l.options
-            o = JSON.parse(c.localize_common_action_events.options)
-            if o.name
-              name = o.name
+            o = JSON.parse(l.options)
+            if o['name']
+              name = o['name']
             end
           end
         end
       end
       if name == nil && c.options
         o = JSON.parse(c.options)
-        if o.name
-          name = o.name
+        if o['name']
+          name = o['name']
         end
       end
       name ||= ''
