c2230259185f28e4b17c7a674e9dc3cc89726335
wzen <wzensum@gmail.com>
2015-01-28 18:38:17 +0900

    タイムラインイベント

--- app/controllers/worktable_controller.rb
+++ app/controllers/worktable_controller.rb
@@ -7,9 +7,16 @@ class WorktableController < ApplicationController
     common_events = CommonActionEvent
                         .includes(:localize_common_action_events)
                         .joins(:locales).merge(Locale.available)
+    # アイテム選択
+    @select_items = select_items(common_events)
+    # メソッド選択
+    @select_methods = select_methods(common_events)
+  end
 
-    @select_forms = []
-    @select_forms << {name: '(Select)', value: ''}
+  private
+  def select_items(common_events)
+    select = []
+    select << {name: '(Select)', value: ''}
     common_events.each do |c|
       name = nil
       unless c.localize_common_action_events.empty?
@@ -30,7 +37,14 @@ class WorktableController < ApplicationController
         end
       end
       name ||= ''
-      @select_forms << {name: name, value: "c#{c.id}"}
+      select << {name: name, value: "c#{c.id}"}
     end
+    return select
+  end
+
+  private
+  def select_methods(common_events)
+    methods = []
+
   end
 end
