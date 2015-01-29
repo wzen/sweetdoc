8efa3262f497acf5452d0537dcbf7c422c5c2488
wzen <wzensum@gmail.com>
2015-01-29 13:59:21 +0900

    タイムラインイベント  一時コミット

--- app/controllers/worktable_controller.rb
+++ app/controllers/worktable_controller.rb
@@ -5,44 +5,8 @@ class WorktableController < ApplicationController
     # Constantの設定
     init_const
 
-    # 共通タイムラインイベントの設定
-    common_action_events = CommonActionEvent
-                        .joins(:locales).merge(Locale.available)
-                        .select('common_action_events.*, localize_common_action_events.options as l_options')
-    # アイテム選択
-    @select_items = select_items(common_action_events)
-
-    # イベント設定値用
-    @action_event_type_ids = []
-    common_action_events.each do |c|
-      @action_event_type_ids << {id: c.id , action_event_type_id: c.action_event_type_id}
-    end
+    @common_action_events = CommonActionEvent.get_commonevents_initworktable
   end
 
-  private
-  def merge_options(common_action_events)
-    options_array = []
-    common_action_events.each do |c|
-      options = {}
-      if c.options
-        options.merge!(JSON.parse(c.options))
-      end
-      if c.l_options
-        options.merge!(JSON.parse(c.l_options))
-      end
-      options_array << {id:c.id, options: options}
-    end
-    return options_array
-  end
 
-  private
-  def select_items(common_action_events)
-    select = []
-    select << {name: '(Select)', value: ''}
-    merged = merge_options(common_action_events)
-    merged.each do |m|
-      select << {name: m[:options]['name'], value: "c#{m[:id]}"}
-    end
-    return select
-  end
 end
--- app/models/common_action_event.rb
+++ app/models/common_action_event.rb
@@ -1,4 +1,22 @@
 class CommonActionEvent < ActiveRecord::Base
   has_many :localize_common_action_events
   has_many :locales, :through => :localize_common_action_events
+
+  def self.get_commonevents_initworktable
+    # 共通タイムラインイベントの設定
+    common_action_events = self.joins(:locales).merge(Locale.available)
+                               .select('common_action_events.*, localize_common_action_events.options as l_options')
+
+    # optionsをハッシュ化
+    common_action_events.each do |c|
+      c.options = JSON.parse(c.options)
+      if c.l_options
+        # optionsのローカライズをマージ
+        c.options.merge!(JSON.parse(c.l_options))
+      end
+    end
+
+    return common_action_events
+  end
+
 end
