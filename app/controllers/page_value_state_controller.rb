a7d696b07d40ec347237500b776ec85e60d2400d
wzen <wzensum@gmail.com>
2015-08-23 08:12:24 +0900

    一時コミット

--- app/controllers/page_value_state_controller.rb
+++ app/controllers/page_value_state_controller.rb
@@ -6,21 +6,7 @@ class PageValueStateController < ApplicationController
     i_page_values = params[Const::ServerStorage::Key::INSTANCE_PAGE_VALUE]
     e_page_values = params[Const::ServerStorage::Key::EVENT_PAGE_VALUE]
     s_page_values = params[Const::ServerStorage::Key::SETTING_PAGE_VALUE]
-
-    begin
-      ActiveRecord::Base.transaction do
-        ip = InstancePagevalue.new(user_id, i_page_values)
-        ip.save!
-        ep = EventPagevalue.new(user_id, e_page_values)
-        ep.save!
-        sp = SettingPagevalue.new(user_id, s_page_values)
-        sp.save!
-      end
-      @message = t('message.database.item_state.save.success')
-    rescue => e
-      # 更新失敗
-      @message = t('message.database.item_state.save.error')
-    end
+    @message = PageValueState.save_state(user_id, i_page_values, e_page_values, s_page_values)
   end
 
   def load_state
