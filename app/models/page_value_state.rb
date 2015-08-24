289fde599a44044d330c7bd29f340a07fb01a1b3
wzen <wzensum@gmail.com>
2015-08-24 18:09:53 +0900

    一時コミット

--- app/models/page_value_state.rb
+++ app/models/page_value_state.rb
@@ -2,39 +2,45 @@ class PageValueState
   def self.save_state(user_id, i_page_values, e_page_values, s_page_values)
     begin
       if i_page_values != 'null' || e_page_values != 'null' || s_page_values != 'null'
+
+        user_page_values = UserPagevalue.where(user_id: user_id).order('updated_at desc').first
+
         ActiveRecord::Base.transaction do
           if i_page_values != 'null'
             ip = InstancePagevalue.new({data: i_page_values})
             ip.save!
+            ip_id = ip.id
           else
-            ip = InstancePagevalue.find(:last)
+            ip_id = user_page_values.instance_pagevalue_id
           end
           if e_page_values != 'null'
             ep = EventPagevalue.new({data: e_page_values})
             ep.save!
+            ep_id = ep.id
           else
-            ep = EventPagevalue.find(:last)
+            ep_id = user_page_values.event_pagevalue_id
           end
           if s_page_values != 'null'
             sp = SettingPagevalue.new({data: s_page_values})
             sp.save!
+            sp_id = sp.id
           else
-            sp = SettingPagevalue.find(:last)
+            sp_id = user_page_values.setting_pagevalue_id
           end
           up = UserPagevalue.new({
                                      user_id: user_id,
-                                     instance_pagevalue_id: ip.id,
-                                     event_pagevalue_id: ep.id,
-                                     setting_pagevalue_id: sp.id
+                                     instance_pagevalue_id: ip_id,
+                                     event_pagevalue_id: ep_id,
+                                     setting_pagevalue_id: sp_id
                                  })
           up.save!
         end
       end
 
-      return t('message.database.item_state.save.success')
+      return I18n.t('message.database.item_state.save.success')
     rescue => e
       # 更新失敗
-      return t('message.database.item_state.save.error')
+      return I18n.t('message.database.item_state.save.error')
     end
   end
 
