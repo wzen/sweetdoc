fd1cc2c4ca8936cf5e9eefc83d1e331e8a6a009a
wzen <wzensum@gmail.com>
2016-03-19 21:46:04 +0900

    Preloadの実行タイミングが遅い場合、Run時に不具合が発生するので、pipelineの中に含めた

--- app/assets/javascripts/pipeline/preload_item.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * Created by susumuwatanabe on 12/7/15.
- */
-
-//= require preload_item/pi_arrow
-//= require preload_item/pi_button
-//= require preload_item/pi_image
-//= require preload_item/pi_text
--- app/assets/javascripts/pipeline/worktable.js
+++ app/assets/javascripts/pipeline/worktable.js
@@ -53,6 +53,10 @@
 //= require item/item_base
 //= require item/css_item_base
 //= require item/canvas_item_base
+//= require preload_item/pi_arrow
+//= require preload_item/pi_button
+//= require preload_item/pi_image
+//= require preload_item/pi_text
 //= require event_page_value/base/base
 //= require event_page_value/item/item
 //= require navbar/navbar
