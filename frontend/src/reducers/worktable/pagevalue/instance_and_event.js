912b79288e01783b53a5b25a9c325b3eb3ae1b1d
Susumu Watatanabe <wzensum@gmail.com>
2017-11-14 10:17:26 +0900

    replace redux (WIP)

--- frontend/src/reducers/worktable/pagevalue/instance_and_event.js
+++ frontend/src/reducers/worktable/pagevalue/instance_and_event.js
@@ -1,12 +1,61 @@
-const defaultEvent = (action) => {
+import Common from '../../../base/common';
 
+const pageKey = (page) => {
+  return `p_${page}`;
+};
+
+const eventKey = (eventNum) => {
+  return `te_${eventNum}`;
+};
+
+const forkKey = (state, action) => {
+  let forkNum = currentForkNum(state, action);
+  return forkNum ? `ef_${forkNum}` : 'master';
+};
+
+const currentEventCount = (state, action) => {
+  try {
+    return state.events[pageKey(action.page)][forkKey(state, action)];
+  } catch (e) {
+    return 0;
+  }
+};
+
+const currentForkNum = (state, action) => {
+  try {
+    return state.events[pageKey(action.page)].fork_num;
+  } catch (e) {
+    return null;
+  }
+};
+
+const _defaultEvent = (action, itemInstanceId) => {
+  return {
+    dist_id: Common.generateId(),
+    id: itemInstanceId,
+    item_class_name: action.item_class_name,
+    item_size_diff: {x: 0, y: 0, w: 0, h: 0},
+    do_focus: true,
+    is_common_event: false,
+    finish_page: false,
+    method_name: '',
+    action_type: '',
+    scroll_point_start: '',
+    scroll_point_end: '',
+    is_sync: '',
+    scroll_enabled_directions: '',
+    scroll_forward_directions: '',
+    eventDuration: '',
+    specificMethodValues: null,
+    modifiable_vars: null
+  };
 };
 
 const createInstance = (state, action) => {
-  let instanceId = test;
+  let instanceId = `i_${action.itemType}_${Common.generateId()}`;
   Object.assign(state, {
     instances: {
-      [action.page]: {
+      [pageKey(action.page)]: {
         [instanceId]: {
 
           ...action.params
@@ -14,8 +63,12 @@ const createInstance = (state, action) => {
       }
     },
     events: {
-      [action.page]: {
-
+      [pageKey(action.page)]: {
+        [forkKey(state, action)]: {
+          [eventKey(parseInt(currentEventCount(state, action)) + 1)]: {
+            ..._defaultEvent(action, instanceId)
+          }
+        }
       }
     }
   });
